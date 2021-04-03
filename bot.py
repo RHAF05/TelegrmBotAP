23671aff6e39f7f60a133d2ece64558ce9d76e12
RHAF <RHAF05@hotmail.com>
2021-04-03 11:41:23 -0500

    ajuste pole

--- bot.py
+++ bot.py
@@ -4,6 +4,9 @@ from flask import Flask, request
 
 import telebot
 
+import datetime
+import sqlite3
+
 TOKEN = os.environ.get('BOT_TOKEN')
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
@@ -11,12 +14,83 @@ server = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+    bot.reply_to(message, 'Hola, ' + message.from_user.first_name)
 
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
-    bot.reply_to(message, message.text)
+    """Pole para AP"""
+    weekno = datetime.datetime.today().weekday()
+    now = datetime.datetime.now()
+    hour = now.hour
+
+    db = sqlite3.connect("data.db")
+    cursor = db.cursor()
+
+    #sacamos las fecha de consulta
+    fecha_ant = "'" + str(now.year) + '-' + str(now.month) + '-' + str(now.day) + "'"
+    fecha_act = datetime.datetime(now.year, now.month, (now.day+1))
+
+    # if weekno < 7:
+    if weekno < 5 and now.hour>=5:
+        if(message.text.lower()=="pole"):
+            user = message.from_user.first_name
+            #Validamos que no se haya hecho la pole
+            datos = cursor.execute("SELECT * FROM poles WHERE pole='pole' AND date >=  '" +str(now.year) + "-" + str(now.month).zfill(2) + "-" + str(now.day).zfill(2) + "'")
+            if len(datos.fetchall())==0:
+                registros = ('pole',user,3.5,now)
+                datos = cursor.execute("INSERT INTO poles(pole,user,points,date) VALUES(?,?,?,?)",registros)
+                db.commit()
+                bot.reply_to(message,
+                    f'Erda bien y tal, {user}\ ha hecho la pole'
+                )
+        elif (message.text.lower()=="plata"):
+            user = message.from_user.first_name
+            # Validamos que se haya hecho la pole
+            datos = cursor.execute("SELECT * FROM poles WHERE pole='pole' AND date >=  '" +str(now.year) + "-" + str(now.month).zfill(2) + "-" + str(now.day).zfill(2) + "'")
+            if len(datos.fetchall())>0:
+                # Validamos que no se haya hecho una plata
+                datos = cursor.execute("SELECT * FROM poles WHERE pole='plata' AND date >=  '" +str(now.year) + "-" + str(now.month).zfill(2) + "-" + str(now.day).zfill(2) + "'")
+                if len(datos.fetchall())==0:
+                    #Validamos que si este usuario ya hizo alguna position, no puede hacer plata
+                    datos = cursor.execute("SELECT * FROM poles WHERE user='"+str(user)+"' AND date >=  '" +str(now.year) + "-" + str(now.month).zfill(2) + "-" + str(now.day).zfill(2) + "'")
+                    if len(datos.fetchall())==0:
+                        registros = ('plata',user,2,now)
+                        datos = cursor.execute("INSERT INTO poles(pole,user,points,date) VALUES(?,?,?,?)",registros)
+                        db.commit()
+                        bot.reply_to(message,
+                            f'muy bien, {user}\ ha hecho la plata'
+                        )
+        elif (message.text.lower()=="bronce"):
+            user = message.from_user.first_name
+            # Validamos que se haya hecho la plata
+            datos = cursor.execute("SELECT * FROM poles WHERE pole='plata' AND date >=  '" +str(now.year) + "-" + str(now.month).zfill(2) + "-" + str(now.day).zfill(2) + "'")
+            if len(datos.fetchall())>0:
+                # Validamos que no se haya hecho una plata
+                datos = cursor.execute("SELECT * FROM poles WHERE pole='bronce' AND date >=  '" +str(now.year) + "-" + str(now.month).zfill(2) + "-" + str(now.day).zfill(2) + "'")
+                if len(datos.fetchall())==0:
+                    #Validamos que si este usuario ya hizo alguna position, no puede hacer plata
+                    datos = cursor.execute("SELECT * FROM poles WHERE user='"+str(user)+"' AND date >=  '" +str(now.year) + "-" + str(now.month).zfill(2) + "-" + str(now.day).zfill(2) + "'")
+                    if len(datos.fetchall())==0:
+                        registros = ('bronce',user,0.5,now)
+                        datos = cursor.execute("INSERT INTO poles(pole,user,points,date) VALUES(?,?,?,?)",registros)
+                        db.commit()
+                        bot.reply_to(message,
+                            f'Algo es algo, {user}\ ha conseguido el bronce'
+                        )
+        elif (message.text.lower()=="fail"):
+            user = message.from_user.first_name
+            registros = ('fail',user,0,now)
+            datos = cursor.execute("INSERT INTO poles(pole,user,points,date) VALUES(?,?,?,?)",registros)
+            db.commit()
+            bot.reply_to(message,
+                f'Al menos lo intento, {user}\ ha conseguido un Fail'
+            )
+    elif (message.text.lower()=="poleprueba" or message.text.lower()=="plataprueba" or message.text.lower()=="bronceprueba" or message.text.lower()=="fail"):
+        user = message.from_user.first_name
+        bot.reply_to(message,
+            f'Deja de molestarme {user} que estas no son horas de estar haciendo {message.text.lower()}'
+        )
 
 
 @server.route('/' + TOKEN, methods=['POST'])
